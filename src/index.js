d2de69c734a0e538f51ccfd922b5a59e30027528
Edis Rahic <rahic.edis@gmail.com>
2025-08-14 21:05:42 -0400

    Updated styling and added a project input box for users to specify their project details and deadline. Implemented functionality for users to open the input box by clicking “Add A New Project” on the sidebar. Added cancel and submit buttons to hide the input box after use.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+
+const domReference = (function () {
+  const addProject = document.querySelector(".add-project-tab");
+  const projectBox = document.querySelector(".add-new-project-section");
+  const cancelBtn = document.querySelector(".cancel-btn");
+  const submitBtn = document.querySelector(".submit-btn");
+  return { addProject, projectBox, cancelBtn, submitBtn };
+})();
+
+(function () {
+  domReference.addProject.addEventListener("click", function () {
+    domReference.projectBox.classList.remove("hidden");
+  });
+
+  domReference.cancelBtn.addEventListener("click", function () {
+    domReference.projectBox.classList.add("hidden");
+  });
+
+  domReference.submitBtn.addEventListener("click", function () {
+    domReference.projectBox.classList.add("hidden");
+  });
+})();
